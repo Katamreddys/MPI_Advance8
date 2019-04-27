e1fdc998367e5443444a098227937d8fe9accac6
Sulekha <skatamre@uncc.edu>
2019-04-27 12:48:46 -0400

    Modifeid plot

--- 8.assignment-mpi-pt-to-pt/pingpong/mpi_ping_pong.cpp
+++ 8.assignment-mpi-pt-to-pt/pingpong/mpi_ping_pong.cpp
@@ -15,24 +15,24 @@ int main (int argc, char* argv[]) {
   MPI_Comm_rank(MPI_COMM_WORLD, &rankMPI);
   MPI_Comm_size(MPI_COMM_WORLD, &sizeMPI);
 
-  int Val;
+  int Va;
   if (rankMPI == 0) {
       //element
-    Val = element;
+    Va = element;
       //send and receive
-    MPI_Send(&Val, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+    MPI_Send(&Va, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
     cout<<"Send from 0 to 1"<<endl;
-    MPI_Recv(&Val, 1, MPI_INT, 1, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+    MPI_Recv(&Va, 1, MPI_INT, 1, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
     cout<<"After adding Recive"<<endl;
-    cout<<"After adding: " <<Val<<endl;
+    cout<<"After adding: " <<Va<<endl;
   } else if (rankMPI == 1) {
       
       //receive add and send
-    MPI_Recv(&Val, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+    MPI_Recv(&Va, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
     cout<<"Received to 1 from 0"<<endl;
 
-    Val = Val + 2;
-    MPI_Send(&Val, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    Va = Va + 2;
+    MPI_Send(&Va, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
     cout<<"Sending to 0 from 1 after adding"<<endl;
 
   }
